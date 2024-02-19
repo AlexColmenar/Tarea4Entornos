7c25b0568608e9d24d8d9486fdf75167b9369c8f
Pablo Montañana Fuentes <pablomontanana@gmail.com>
2024-02-19 23:33:13 +0100

    Inicio

---
+++ src/Ejercicio1/Main.java
@@ -0,0 +1,39 @@
+package Ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        eCesta cesta;
+
+        cesta = new eCesta("Colmenar Arevalo", 100, "77599310R", 10.20);
+
+        try {
+
+            cesta.comprar(50);
+
+        } catch (Exception e) {
+
+            System.out.println("No se puede pagar  ");
+
+        }
+
+        try {
+
+            System.out.println("Solicitar actualización de bono");
+
+            cesta.ActualizarBono(100);
+
+        } catch (Exception e) {
+
+            System.out.println("Error al recargar");
+
+        }
+
+        double bonoactual = cesta.bonificacion();
+
+        System.out.println("Su bono actual es  " + bonoactual);
+
+    }
+
+}
